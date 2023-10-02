32d80a3a14f9bca448e1e497b620425ae8797786
Arman Moaveni <134084996+arimoa@users.noreply.github.com>
2023-10-02 12:01:55 +0330

    Update main.js

--- main.js
+++ main.js
@@ -1,36 +1,29 @@
-var containerEl=document.getElementById("container");
+const containerEl = document.getElementById("container");
 function boxCreator() {
-    for (let index = 0; index < 30; index++) {
-        var colorBoxEl = document.createElement("div");
-        colorBoxEl.classList.add("colorBox");
-        containerEl.appendChild(colorBoxEl);
-        
-    }
+  for (let index = 0; index < 30; index++) {
+    const colorBoxEl = document.createElement("div");
+    colorBoxEl.classList.add("colorBox");
+    containerEl.appendChild(colorBoxEl);
+  }
 }
-
 boxCreator();
-
-var j;
-
+let j;
 const colorLetters = "0123456789abcdef";
 
 function randomCode() {
-    var colorCode = "";
-    for (let index1 = 0; index1 < 6; index1++) {
-        j = Math.floor(Math.random()*16);
-        colorCode = colorCode.concat(colorLetters[j]);
-    }
-    
-    return colorCode;
+  var colorCode = "";
+  for (let index1 = 0; index1 < 6; index1++) {
+    j = Math.floor(Math.random() * 16);
+    colorCode = colorCode.concat(colorLetters[j]);
+  }
+  return colorCode;
 }
 
+const finalBox = document.getElementsByClassName("colorBox");
 
-var finalBox=document.getElementsByClassName("colorBox");
-
-for (let k=0; k <30; k++) {
-    var currentColor = "";
-     finalBox[k].innerHTML = randomCode();
-     currentColor = currentColor.concat("#",finalBox[k].innerHTML);
-     finalBox[k].style.backgroundColor = currentColor;
-
+for (let k = 0; k < 30; k++) {
+  let currentColor = "";
+  finalBox[k].innerHTML = randomCode();
+  currentColor = currentColor.concat("#", finalBox[k].innerHTML);
+  finalBox[k].style.backgroundColor = currentColor;
 }
